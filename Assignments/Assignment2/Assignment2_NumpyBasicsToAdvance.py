f4d97cfa24e1707fb95896b5faedf6a0b4d5bb5b
FarhanIqbal2003 <55238562+FarhanIqbal2003@users.noreply.github.com>
2020-12-30 13:47:04 +0500

    Update Assignment2_NumpyBasicsToAdvance.py
    task18

--- Assignments/Assignment2/Assignment2_NumpyBasicsToAdvance.py
+++ Assignments/Assignment2/Assignment2_NumpyBasicsToAdvance.py
@@ -309,7 +309,7 @@ def function18():
     # count values of "students" are exist in "piaic"
     piaic = np.arange(100)
     students = np.array([5,20,50,200,301,7001])
-    x = # Write you code Here
+    x = np.isin(students, piaic, 1, 0).sum()
     return x
 
     #Expected output: 3
